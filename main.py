30abb765bcfc49067f3fb6bb268889e42014f2ab
EwenKum <evgeniikumitskii@gmail.com>
2024-09-28 20:24:57 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello world")
+#1st program
+print(9 ** 0.5 * 5)
+
+#2st program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3st program
+num1 = 2 * 2 + 2
+num2 = 2 * (2 + 2)
+print(num1)
+print(num2)
+print(num1 == num2)
+
+#4st program
+num_str = '123.456'
+print(int(float(num_str) * 10) % 10)
