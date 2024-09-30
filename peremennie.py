62f3f50ade3e50b7542473b581663f12c4159562
EwenKum <evgeniikumitskii@gmail.com>
2024-09-30 17:33:08 +0300

    Add files via upload

---
+++ peremennie.py
@@ -0,0 +1,5 @@
+complete_tasks = 12
+all_time = 1.5
+name_course = 'Python'
+print('Курс:', name_course + ',', 'всего задач:', str(complete_tasks) + ',', 'затрачено часов:', str(all_time) + ',', 'среднее время выполнения', all_time / complete_tasks, 'часа.')
+
