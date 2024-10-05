d09fc0e4492cb83ef626be84e7045dcf933eca11
EwenKum <evgeniikumitskii@gmail.com>
2024-10-05 13:16:02 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,22 @@
+#dict
+my_dict = {'johny1197@mail.ru': 'Bgffg8556bgf78!',
+           'novelkin@gmail.com': 'cdScd74bgdE786!'
+           }
+print(my_dict)
+print(my_dict['johny1197@mail.ru'])
+print(my_dict.get('ferfwgg@yandex.ru'))
+my_dict.update({'ferfwgg@yandex.ru': 'Damba469voznes44444',
+                      'veli4ie@rambler.ru': 'VFsds545!vdf'
+                      })
+johny_value = my_dict.pop('johny1197@mail.ru')
+print(johny_value)
+print(my_dict)
+
+#set
+my_set = {25, 98, 35, 25, 'Vasya', 58, 58, 98, 'Kolya', 35, 25, 'Vasya', 58}
+print(my_set)
+my_set.add(22)
+my_set.add(36)
+my_set.remove('Vasya')
+print(my_set)
+
